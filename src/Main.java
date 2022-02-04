f2cb407fded8e51ef14b6c85608e128cea0de0c6
PS <89146019257@mail.ru>
2022-02-04 22:33:16 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // входные данные
+        int initialamount = 100;
+        int amountplus = 1600;
+        int bonus = amountplus / 100;
+        int bonus2 = 0;
+        int bonus3 = bonus + 1;
+        int total = initialamount + amountplus + bonus3;
+        int total2 = initialamount + amountplus + bonus2;
+
+
+        //Логика
+        if (amountplus > 1000) {
+            System.out.println("Итоговый счет");
+            System.out.println(total);
+        }
+        if (amountplus < 1000) {
+            System.out.println("Итоговый счет");
+            System.out.println(total2);
+        }
+    }
+}
